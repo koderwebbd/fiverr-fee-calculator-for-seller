01bae8583faf4135b0ddd95522b5f40a0aa0c43f
koderwebbd <73799573+koderwebbd@users.noreply.github.com>
2024-04-17 14:03:28 +0600

    Update main.js
    add new functions

--- main.js
+++ main.js
@@ -2,50 +2,167 @@
 //https://www.youtube.com/watch?v=BT4WzyT2g8k
 const priceInput = document.querySelector(".priceValue");
 const tipInput = document.querySelector(".tipValue");
-const symbol = document.querySelector(".symbol");
+const symbol = document.querySelectorAll(".symbol");
+
 const earn = document.querySelector(".earn");
-const fiverrTakes = document.querySelector(".fiverr");
+const fiverrTakesFromSeller = document.querySelector(".seller_from_fiverr");
+
 const buyerPaid = document.querySelector(".buyer-paid");
+const fiverrTakesFromBuyer = document.querySelector(".buyer_from_fiverr");
+
+const fiverRevenue = document.querySelector(".fiver_revenue");
 
 const tip = document.querySelector(".buyer-paid");
 
 function calculator(){
     
     if ( priceInput.value > 0 || tipInput.value> 0 ) {
-        let TotalPrice = priceInput.value * 1.00;
-        let TotalTip = tipInput.value * 1.00;
+        let inputPrice = priceInput.value * 1.00;
+        let inputTip = tipInput.value * 1.00;
 
-        let TotalAmount = TotalPrice + TotalTip;
-        calculatorCalculation(TotalAmount)
+        var calculatorCalculationData = calculatorCalculation(inputPrice, inputTip);
+        var fiverrTakesData = fiverrTakes(inputPrice, inputTip);
+        var buyer_order_conditionData = buyer_order_condition(inputPrice, inputTip);
 
+        var sellerEarning = calculatorCalculationData[0] + calculatorCalculationData[1];
+        var FiverrSellerEarning = fiverrTakesData[0] + fiverrTakesData[1];
+        var bueryPaidMoney = buyer_order_conditionData[0] + buyer_order_conditionData[2];
+        var FiverrBuyerEarning = buyer_order_conditionData[1] + buyer_order_conditionData[3];
+        var TotalFiverrRevenue = FiverrSellerEarning + FiverrBuyerEarning;
+        
+        sellerEarning = typeof sellerEarning === 'number' ? sellerEarning.toFixed(2) : sellerEarning;
+        FiverrSellerEarning = typeof FiverrSellerEarning === 'number' ? FiverrSellerEarning.toFixed(2) : FiverrSellerEarning;
+        bueryPaidMoney = typeof bueryPaidMoney === 'number' ? bueryPaidMoney.toFixed(2) : bueryPaidMoney;
+        FiverrBuyerEarning = typeof FiverrBuyerEarning === 'number' ? FiverrBuyerEarning.toFixed(2) : FiverrBuyerEarning;
+        TotalFiverrRevenue = typeof TotalFiverrRevenue === 'number' ? TotalFiverrRevenue.toFixed(2) : TotalFiverrRevenue;
+
+
+        //calculation_render(earnings, sellerCommitionF, buyerPaid, buyerCommitionF, FtotalRevinue )
+        calculation_render(sellerEarning, FiverrSellerEarning, bueryPaidMoney, FiverrBuyerEarning, TotalFiverrRevenue )
+
+
+    }else{
+        symbol[0].style.display = 'none';
+        symbol[1].style.display = 'none';
+        
+        earn.innerHTML = '-'; 
+            fiverrTakesFromSeller.innerHTML = '-'; 
+            buyerPaid.innerHTML = '-'; 
+            fiverrTakesFromBuyer.innerHTML = '-'; 
+            fiverRevenue.innerHTML = '-';  
+            event.currentTarget.blur();
     }
 }
-function calculatorCalculation(money){
-     
+let commissionPercentage = 0.20; 
+let sellerEarningCommition = 1 - commissionPercentage;
+
+let serviceCharge = 0.055;
+let additionalFee = 2.5;
+
+function calculatorCalculation(money, tip){
+    if (money) {
+        symbol[0].style.display = 'block';
+    }else{
+        symbol[1].style.display = 'block';
+    }
+    
+
+    //Your earnings
+    var earnings = money * sellerEarningCommition;
+    var earningsTip = tip * sellerEarningCommition;
+
+    //earn.innerHTML = earnings;
+    //earn.innerHTML = earningsTip;
+    //calculation_render(earnings, sellerCommitionF, buyerPaid, buyerCommitionF, FtotalRevinue )
+
+    return [earnings, earningsTip];
+}
+function fiverrTakes(money, tip){
+ 
+    //Calculate earnings from seller fiverr 
+    var fiverrEarningSeller = money * commissionPercentage;
+    fiverrTakesFromSeller.innerHTML = fiverrEarningSeller;
+
+
+    //Calculate earnings from seller tip fiverr 
+    var fiverrEarningSellerTip = tip * commissionPercentage;
+    fiverrTakesFromSeller.innerHTML = fiverrEarningSellerTip;
+
+    // Return earnings as an object
+    return [fiverrEarningSeller, fiverrEarningSellerTip];
+}
+function buyer_order_condition(money, tip){
+    var buyer_order;
+    var moneyFromBuyer;
+
+    if (money > 0 && money < 75) {
+        //rececived buyer paid 
+        buyer_order = money + ( money * serviceCharge) + additionalFee;
+        
+        // fiverr buyer from fiverr       
+        moneyFromBuyer =  money * serviceCharge + additionalFee;
+    }else{
+        //rececived buyer paid
+        buyer_order = money + ( money * serviceCharge);
+
+        // fiverr buyer from fiverr  
+        moneyFromBuyer = money * serviceCharge
+    }
+
+    var buyer_orderTip;
+    var moneyFromBuyerTip;
+
+    if (tip > 0 && tip < 75) {
+        //rececived buyer paid 
+        buyer_orderTip = tip + ( tip * serviceCharge) + additionalFee;
+        
+        // fiverr buyer from fiverr       
+        moneyFromBuyerTip =  tip * serviceCharge + additionalFee;
+    }else{
+        //rececived buyer paid
+        buyer_orderTip = tip + ( tip * serviceCharge);
+
+        // fiverr buyer from fiverr  
+        moneyFromBuyerTip = tip * serviceCharge
+    }
+
+    // buyerPaid.innerHTML = buyer_order;
+    // fiverrTakesFromBuyer.innerHTML = moneyFromBuyer;
    
-    symbol.style.display = 'block';
+   return [buyer_order, moneyFromBuyer, buyer_orderTip, moneyFromBuyerTip]
+}
 
-    let commissionPercentage = 0.20 
-    let commission = 1 - commissionPercentage;
-    let total = money * commission;
+ 
+function calculation_render(earnings, sellerCommitionF, buyerPaids, buyerCommitionF, FtotalRevinue ){
 
-    earn.innerHTML = '$'+total.toFixed(2);
-    fiverrTakes.innerHTML = '$'+(money * commissionPercentage).toFixed(2);
+    earn.innerHTML = earnings; 
+    fiverrTakesFromSeller.innerHTML = sellerCommitionF; 
+    buyerPaid.innerHTML = buyerPaids; 
+    fiverrTakesFromBuyer.innerHTML = buyerCommitionF; 
+    fiverRevenue.innerHTML = FtotalRevinue; 
 
-    buyerPaid.innerHTML = '$'+(total / commission).toFixed(2);
-
+    
 }
 priceInput.addEventListener("keypress", (event) => {
     if(event.target.value < 1){
-       // console.log('under 0')
-        console.log(event.which)
+        //console.log(event.which)
+        //event.currentTarget.blur();
+        symbol[0].style.display = 'none';
+        symbol[1].style.display = 'none';
         if (event.which === 48 || event.which === 96 ) {
-            console.log('under1')
-            return;
-            
+            event.currentTarget.blur();
+            event.target.value = '';
+            symbol[0].style.display = 'none';
+            symbol[1].style.display = 'none';
+            earn.innerHTML = '-'; 
+            fiverrTakesFromSeller.innerHTML = '-'; 
+            buyerPaid.innerHTML = '-'; 
+            fiverrTakesFromBuyer.innerHTML = '-'; 
+            fiverRevenue.innerHTML = '-';  
+        
         }
 
     }
    
-  });
+ });
   
